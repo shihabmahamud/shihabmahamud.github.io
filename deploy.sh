23911076adef4dd256d43fc7f3ea4d84b455de3b
Shihab Mahamud <shihabmahamud02@gmail.com>
2021-03-07 11:03:57 +0600

    update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ current_dir=$PWD
 cd src
 hugo -d ../
 cd $current_dir
-git add . && echo "git adding... Done." && read -p "Please enter commit massages: " com_mas && git commit -m "$com_mas" && echo "git commiting... Done." && git push && echo "git pushing.... Done."
+git add . && echo "git adding... Done." && read -p "Commit Massages: " com_mas && git commit -m "$com_mas" && echo "git commiting... Done." && git push && echo "git pushing.... Done."
