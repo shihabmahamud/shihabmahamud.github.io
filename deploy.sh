3705a23ab14c00226150dae6867fa754c43dc4e1
Shihab Mahamud <shihabmahamud02@gmail.com>
2021-03-12 04:48:00 +0600

    add asciicast in cph tool docs

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-# rm -rf about about-hugo about-us archives categories contact img index.html index.xml p page post scss search sitemap.xml tags ts 404.html \
-current_dir=$PWD \
-&& cd src \
-&& hugo -d ../ \
-&& cd $current_dir \
-&& echo "git adding..." \
-&& git add . \
-&& echo "git commiting..." \
-&& read -p "Massages: " com_mas \
-&& git commit -m "$com_mas" \
-&& echo "git pushing..." \
-&& git push
+rm -rf about about-hugo about-us archives categories contact img index.html index.xml p page post scss search sitemap.xml tags ts 404.html \
+# current_dir=$PWD \
+# && cd src \
+# && hugo -d ../ \
+# && cd $current_dir \
+# && echo "git adding..." \
+# && git add . \
+# && echo "git commiting..." \
+# && read -p "Massages: " com_mas \
+# && git commit -m "$com_mas" \
+# && echo "git pushing..." \
+# && git push
